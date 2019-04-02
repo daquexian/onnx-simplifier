ed5c1b47710584b5000a8553196d48cd667dcdb9
daquexian <daquexian566@gmail.com>
2019-04-02 12:46:28 +0800

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='onnx-simplifier',
-    version='0.1.0',
+    version='0.1.1',
     description='Simplify your ONNX model',
     author='daquexian',
     author_email='daquexian566@gmail.com',
