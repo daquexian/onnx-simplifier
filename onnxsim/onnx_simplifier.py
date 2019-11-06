d02a1406e46b0fc8ad821544d817bb6e5fafa40c
daquexian <daquexian566@gmail.com>
2019-11-07 07:39:48 +0800

    Disable fuse_consecutive_concats optimizier due to https://github.com/daquexian/onnx-simplifier/issues/31

--- onnxsim/onnx_simplifier.py
+++ onnxsim/onnx_simplifier.py
@@ -184,7 +184,9 @@ def optimize(model: onnx.ModelProto) -> onnx.ModelProto:
                                             'eliminate_nop_monotone_argmax', 'eliminate_nop_pad',
                                             'extract_constant_to_initializer', 'eliminate_unused_initializer',
                                             'eliminate_nop_transpose', 'fuse_add_bias_into_conv', 'fuse_bn_into_conv',
-                                            'fuse_consecutive_concats', 'fuse_consecutive_log_softmax',
+                                            # https://github.com/daquexian/onnx-simplifier/issues/31
+                                            # 'fuse_consecutive_concats', 
+                                            'fuse_consecutive_log_softmax',
                                             'fuse_consecutive_reduce_unsqueeze', 'fuse_consecutive_squeezes',
                                             'fuse_consecutive_transposes', 'fuse_matmul_add_bias_into_gemm',
                                             'fuse_pad_into_conv', 'fuse_transpose_into_gemm'],
