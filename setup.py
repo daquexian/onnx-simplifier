8098167602c8757490e910be5f40dd36594bfa28
daquexian <daquexian566@gmail.com>
2023-03-21 18:56:32 +0800

    refine universal2 package support
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ SRC_DIR = os.path.join(TOP_DIR, 'onnxsim')
 CMAKE_BUILD_DIR = os.path.join(TOP_DIR, '.setuptools-cmake-build')
 
 WINDOWS = (os.name == 'nt')
+MACOS = sys.platform.startswith("darwin")
 
 CMAKE = find_executable('cmake')
 
@@ -170,6 +171,11 @@ class cmake_build(setuptools.Command):
                     cmake_args.extend(['-A', 'x64', '-T', 'host=x64'])
                 else:
                     cmake_args.extend(['-A', 'Win32', '-T', 'host=x86'])
+            if MACOS:
+                # Cross-compile support for macOS - respect ARCHFLAGS if set
+                archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+                if archs:
+                    cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
             if ONNX_ML:
                 cmake_args.append('-DONNX_ML=1')
             if ONNX_VERIFY_PROTO3:
