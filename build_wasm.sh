e612ade24a404ba37c7fe6c43f892a8c04e6b6c9
take-cheeze <takechi101010@gmail.com>
2025-06-04 18:45:21 +0900

    Build fixes

--- build_wasm.sh
+++ build_wasm.sh
@@ -8,13 +8,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WITH_NODE_RAW_FS=${1:-OFF}
 
 cd $SCRIPT_DIR
-pushd third_party/onnxruntime/cmake/external/protobuf/cmake
-mkdir -p build
-cd build
-cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -GNinja ..
-ninja protoc
-PROTOC=`pwd`/protoc
-popd
+if which protoc ; then
+    PROTOC=$(which protoc)
+else
+    pushd third_party/onnxruntime/cmake/external/protobuf/cmake
+    mkdir -p build
+    cd build
+    cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -GNinja ..
+    ninja protoc
+    PROTOC=`pwd`/protoc
+    popd
+fi
 
 mkdir -p build-wasm-node-$WITH_NODE_RAW_FS
 cd build-wasm-node-$WITH_NODE_RAW_FS
